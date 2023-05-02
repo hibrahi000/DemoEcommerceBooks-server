c26b3d7627b132e9f5a99c5b2d1b15276ad13512
Hashmat ibrahimi <hashmatibrahimi@Hashmats-MacBook-Pro.local>
2023-05-02 19:59:09 -0400

    Initail application functionality - Release-1.0

---
+++ src/main/java/com/hash/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,25 @@
+package com.hash.ecommerce.config;
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.hash.ecommerce.entity.Product;
+
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST };
+
+        // disable HTTP methods for PUT, POST, and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, HttpMethods) -> HttpMethods.disable(theUnsupportedActions));
+    }
+
+}
